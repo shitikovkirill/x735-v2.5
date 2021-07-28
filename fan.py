0828f59df4dc65c96f09730c739a5ecdc68eb953
Kirill Shitikov <sh.kiruh@gmail.com>
2021-07-29 00:55:09 +0300

    Add cli command

---
+++ fan.py
@@ -0,0 +1,44 @@
+import pigpio
+
+
+class Fan:
+    
+    __slots__ = ['pin', 'pwm']
+
+    def __init__(self):
+        self.pin = 13
+        self.pwm = pigpio.pi()
+        self.pwm.set_mode(self.pin, pigpio.OUTPUT)
+        self.pwm.set_PWM_frequency(self.pin, 25000)
+        self.pwm.set_PWM_range(self.pin, 100)
+        
+    def set_speed(self, speed):
+        self.pwm.set_PWM_dutycycle(self.pin, speed)
+
+
+def get_cpu_temp():
+    """
+    Get temperature from cpu
+    """
+    with open("/sys/class/thermal/thermal_zone0/temp") as file:
+        temp = float(file.read()) / 1000.00
+        temp = float('%.2f' % temp)
+    return temp
+
+
+def get_fun_speed(temp):
+    """
+    Calculate fun speed by temperature
+    """
+    fun_speed = 0
+    if 50 <= temp > 30:
+        fun_speed = 40
+    elif 55 <= temp > 50:
+        fun_speed = 50
+    elif 60 <= temp > 55:
+        fun_speed = 75
+    elif 65 <= temp > 60:
+        fun_speed = 90
+    elif temp > 65:
+        fun_speed = 100
+    return fun_speed
---
+++ main.py
@@ -0,0 +1,36 @@
+import click
+import time
+from fan import Fan, get_cpu_temp, get_fun_speed
+
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+@click.pass_context
+def fan(ctx, debug):
+    click.echo("Debug mode is {}".format('on' if debug else 'off'))
+    ctx.ensure_object(dict)
+    ctx.obj['DEBUG'] = debug
+
+
+@fan.command()
+@click.pass_context
+def run(ctx):
+    """Script for running fan"""
+    fan = Fan()
+    while True:
+        temp = get_cpu_temp()
+        if ctx.obj['DEBUG']:
+            click.echo("Temperatura {}".format(temp))
+            
+        fun_speed = get_fun_speed(temp)
+        if ctx.obj['DEBUG']:
+            click.echo("Fun speed {}".format(fun_speed))
+        
+        fan.set_speed(fun_speed)
+        time.sleep(1)
+
+
+@fan.command()
+@click.pass_context
+def info(ctx):
+    """Print fan debug info"""
