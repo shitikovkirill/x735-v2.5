2006e0eecd0fda5b0cfbc9a545b78bf2ca257f24
Shitikov Kirill <k.shitikov@itransition.com>
2021-08-01 16:41:42 +0300

    Update packages

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ import setuptools
 
 # read the contents of your README file
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
+
+git_url = "https://github.com/shitikovkirill/x735-v2.5"
 
 setuptools.setup(
     name="x735-v2.5",
-    version="0.1.3",
+    version="0.1.4",
     description="Library for board x735",
     author="sh.kiruh@gmail.com",
     install_requires=[
@@ -18,11 +21,12 @@ setuptools.setup(
     ],
     extras_require={"dev": ["pre-commit"]},
     python_requires=">=3.7",
+    packages=["x735"],
     entry_points={
-        "console_scripts": ["x735fan=main:fan"],
+        "console_scripts": ["x735fan=x735.main:fan"],
     },
-    url='https://github.com/shitikovkirill/x735-v2.5',
-    download_url='https://github.com/shitikovkirill/x735-v2.5/archive/refs/heads/main.zip',
+    url=git_url,
+    download_url="{}/archive/refs/heads/main.zip".format(git_url),
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
 )
