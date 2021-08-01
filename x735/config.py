0159afdd8854e309703d10f0d3ce6efe1ee460c3
Shitikov Kirill <k.shitikov@itransition.com>
2021-08-01 16:42:04 +0300

    Update packages

---
+++ x735/config.py
@@ -0,0 +1,31 @@
+fun_speed_range = [
+    {
+        "mint": 30,
+        "maxt": 50,
+        # percent of power
+        "speed": 40,
+    },
+    {
+        "mint": 50,
+        "maxt": 55,
+        # percent of power
+        "speed": 50,
+    },
+    {
+        "mint": 55,
+        "maxt": 60,
+        # percent of power
+        "speed": 75,
+    },
+    {
+        "mint": 60,
+        "maxt": 65,
+        # percent of power
+        "speed": 90,
+    },
+    {
+        "mint": 65,
+        # percent of power
+        "speed": 100,
+    },
+]
