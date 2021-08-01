22f117ebce2d2a664cf71df010c0c69db47a9da6
Kirill Shitikov <sh.kiruh@gmail.com>
2021-08-01 16:08:17 +0300

    Fix service

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 import setuptools
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="x735-v2.5",
-    version="0.1.1",
+    version="0.1.2",
     description="Library for board x735",
     author="sh.kiruh@gmail.com",
     install_requires=[
@@ -11,10 +17,11 @@ setuptools.setup(
         "click==8.0.1",
     ],
     extras_require={"dev": ["pre-commit"]},
-    classifiers=[
-        "x735 board",
-    ],
     entry_points={
         "console_scripts": ["x735fan=main:fan"],
     },
+    url='https://github.com/shitikovkirill/x735-v2.5',
+    download_url='https://github.com/shitikovkirill/x735-v2.5/archive/refs/heads/main.zip',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
